6c5c72e1a6e42b2cb4b46acd3c7924b355843441
Filipp <sap.phil.v@mail.ru>
2024-05-12 23:31:12 +0300

    "написана" tomato_stdlib

--- tomato_stdlib/src_asm/stdlib.nasm
+++ tomato_stdlib/src_asm/stdlib.nasm
@@ -9,6 +9,18 @@ _exit:
             syscall
 
 global _write
+; void _write(long unsigned str_len, const char *str_ptr)
+; rdi - str_len
+; rsi - str_ptr
 _write:
-            
-            mov rax, 1
+            push rdi ; str_len
+
+            mov rax, 1 ; syscall num
+            mov rdi, 1 ; stdout
+            ; rsi already in place
+            pop rdx
+            syscall
+
+            ret
+
+
--- tomato_stdlib/src_c/stdlib.cpp
+++ tomato_stdlib/src_c/stdlib.cpp
@@ -1,9 +1,32 @@
-extern "C" double input()
-{
+#include "stdio.h"
 
+extern "C" void _write(long unsigned str_len, const char *str_ptr);
+extern "C" void print_str(const char *str);
+extern "C" void print_num(double x);
+extern "C" double input();
+
+extern "C" void print_str(const char *str)
+{
+    const char *curr = str;
+    long unsigned str_len = 0;
+
+    while (*curr != '\0')
+    {
+        str_len++;
+        curr++;
+    }
+    
+    _write(str_len, str);
 }
 
 extern "C" void print_num(double x)
 {
+    printf("%g\n", x);
+}
 
+extern "C" double input()
+{
+    double res = 0;
+    scanf("%lf", &res);
+    return res;
 }
